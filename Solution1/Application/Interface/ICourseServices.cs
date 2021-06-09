bbad1ae873bd1f2ddb462e0ca8368e404aa03ef0
omid salangi <omidsalangi@outlook.com>
2021-06-10 02:48:22 +0430

    add view models and repositories

---
+++ Solution1/Application/Interface/ICourseServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.ViewModel;
+namespace Application.Interface
+{
+    public interface ICourseServices
+    {
+        CourseViewModel getcCourses();
+    }
+}
---
+++ Solution1/Domain/Interface/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Interface
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Courses> getCourses();
+    }
+}
