fd9e35eaa906b838a20c19bb30ec6906db5e86f1
omid salangi <omidsalangi@outlook.com>
2021-06-10 01:33:17 +0430

    create Course Model

---
+++ Solution1/Cleanarch.Domain/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
