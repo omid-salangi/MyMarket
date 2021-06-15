f7e75d6a6d57b28932d0a051241dce36ccfef2d2
omid salangi <omidsalangi@outlook.com>
2021-06-16 01:36:13 +0430

    add repository and services of course

---
+++ Solution1/Application/Services/CourseService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interface;
+using Application.ViewModel;
+using Domain.Interface;
+using Domain.Models;
+
+namespace Application.Services
+{
+    public class CourseService : ICourseServices
+    {
+        private ICourseRepository _CourseRepository;
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            this._CourseRepository = courseRepository;
+        }
+
+        public CourseViewModel getcCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _CourseRepository.getCourses() // object initializer 
+            };
+        }
+    }
+}
---
+++ Solution1/CleanArch.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArch.Data.Context;
+using Domain.Interface;
+using Domain.Models;
+
+namespace CleanArch.Data.Repository
+{
+    public class CourseRepository:ICourseRepository // in model repositories
+    {
+        private UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            this._context = context;
+        }
+        public IEnumerable<Courses> getCourses()
+        {
+            return _context.Courses; // it will return a iqueryable
+        }
+    }
+}
