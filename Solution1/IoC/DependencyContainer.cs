467f524d8e23e02d029c9a286dacb434c70f28c6
omid salangi <omidsalangi@outlook.com>
2021-06-16 01:57:57 +0430

    add dependencies to IoC

---
+++ Solution1/IoC/DependencyContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interface;
+using Application.Services;
+using CleanArch.Data.Repository;
+using Domain.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            // application layer
+            service.AddScoped<ICourseServices, CourseService>();
+
+            //data layer
+            service.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
