83608d80cc67fe77c235ce28c5351afbfc9d2854
omid salangi <omidsalangi@outlook.com>
2021-06-28 01:23:51 +0430

    create changes in db and course controller

---
+++ Solution1/MyMarket.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Interface;
+using Application.ViewModel;
+
+namespace MyMarket.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseServices _courseServices;
+
+        public CourseController(ICourseServices courseServices)
+        {
+            _courseServices = courseServices;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseServices.getcCourses();
+            return View(model);
+        }
+    }
+}
